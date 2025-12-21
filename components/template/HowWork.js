69e23aa0469fc08e960807b20a2e203764274340
Roqhaye Halimi <roqiahalimi95@gmail.com>
2025-12-21 21:56:01 +0330

    completed how work component in desktop mod!

--- components/template/HowWork.js
+++ components/template/HowWork.js
@@ -4,63 +4,59 @@ function HowWork() {
   return (
     <div
       className="
-    w-full h-[396px] bg-indigo-600 pt-32
-    [clip-path:polygon(0%_0%,100%_25%,100%_100%,0%_100%)]
+    w-full h-[1124px] bg-gray-100 mt-24 pt-32
+    [clip-path:polygon(0%_0%,100%_10%,100%_100%,0%_100%)]
   "
     >
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut in quis
-      perspiciatis vero ipsam similique. Consectetur rerum recusandae dolorum,
-      placeat veniam porro similique molestiae nisi, dicta voluptatum quia unde
-      est soluta atque adipisci? Iste dolor quidem magnam sit eum? Laudantium
-      deleniti ut amet veritatis culpa voluptate ex ipsam delectus pariatur esse
-      sunt ab accusantium cumque placeat vero odit, eum facere quibusdam, beatae
-      ducimus, officiis eos. Aspernatur sit ullam earum est ipsa quibusdam
-      doloribus laborum saepe rem velit accusamus nesciunt autem repudiandae
-      dignissimos deleniti quisquam, sint fuga vitae. Inventore suscipit veniam
-      dolores fugiat repudiandae enim expedita vero possimus voluptatem ea!
-      Illum quia eaque architecto earum eum, ipsam accusamus saepe voluptatem
-      sed eveniet fugit dolores, iusto quas deserunt alias tempore! Fugit autem
-      voluptatem officia porro unde, blanditiis nulla consequuntur. Quam aliquid
-      ipsam nihil. Numquam at id, veritatis perferendis tempore fugiat maiores,
-      hic, quos repudiandae blanditiis corporis. Quos totam accusamus asperiores
-      reiciendis odio suscipit? Similique doloremque pariatur dolores, expedita
-      eaque facilis! Quibusdam, porro iste? Asperiores, quam. Iure minus, animi
-      provident itaque maiores, esse ipsa reiciendis iste corrupti eius tenetur
-      consequatur suscipit beatae voluptas natus. Eligendi, iste fugit?
-      Voluptates voluptatum quasi debitis ex sapiente delectus corrupti deleniti
-      non fugit magni enim nulla cupiditate natus ipsa excepturi corporis minus
-      possimus blanditiis hic, iste ab aspernatur totam sed minima? Itaque
-      officia delectus suscipit natus laborum minus porro, tempore mollitia
-      ipsam sapiente totam nemo magni impedit, aspernatur velit libero ullam
-      repellat inventore. Assumenda eveniet quidem quaerat fugit id amet at
-      deleniti nihil aut eaque, illum voluptatum sint, modi natus dolorum
-      sapiente adipisci possimus repellat quod! Libero repellendus a, molestias
-      ipsam voluptatum quis ullam possimus dolor eaque dignissimos placeat
-      ducimus tenetur rerum accusamus vel voluptatibus ipsa corporis. Corrupti
-      magni placeat nisi laborum consequatur? Doloribus cum sunt aperiam aliquid
-      officia, repudiandae ad incidunt voluptatibus natus reiciendis alias, eum
-      accusamus id, dolorem cupiditate quas delectus? Asperiores aperiam, iusto
-      natus nostrum corporis odio minus explicabo? Ab libero voluptatum iure
-      officiis quod, voluptatibus obcaecati error possimus alias. Deserunt
-      dolorum cupiditate cumque blanditiis maxime aut quaerat obcaecati aperiam
-      a expedita dignissimos, voluptates debitis in ad! Voluptatum sequi nulla
-      nesciunt consequuntur repudiandae totam? Ex illo similique, quos vitae
-      accusantium inventore, reprehenderit dolorem eum repudiandae consequatur,
-      dolor necessitatibus non facilis? Atque, corrupti? Iusto ipsa libero
-      corrupti dignissimos voluptas. Quam laboriosam similique mollitia, dolores
-      illum fugiat maxime nam laudantium harum praesentium atque cupiditate
-      accusamus sapiente odio inventore reprehenderit aliquam. Incidunt nihil
-      rem nesciunt consectetur perspiciatis praesentium placeat debitis a
-      maxime, magnam animi nostrum reiciendis. Quae maxime nemo quos sunt ipsam
-      itaque asperiores. Tenetur, eos? Aut commodi officiis rerum distinctio,
-      deserunt id explicabo. Dolorum quis perspiciatis aliquid reprehenderit
-      dolor earum modi id nulla fugit, quaerat veritatis ea, distinctio qui
-      corporis voluptate eius iusto quibusdam, incidunt itaque nostrum beatae
-      quo veniam? Vitae corrupti sit ullam eaque culpa repellat numquam ipsam.
-      Rerum sequi obcaecati, debitis eos sit vel velit sed vitae quo suscipit
-      dignissimos et. Odio quidem dicta quia reiciendis ab natus dolore
-      repudiandae distinctio nihil, accusantium doloribus quam voluptatibus,
-      pariatur cupiditate, tenetur excepturi. Aperiam autem enim omnis iure.
+      <div className="w-full px-4 mx-auto max-w-7xl flex flex-col items-center justify-center">
+        <h1 className="text-indigo-900 text-3xl font-extrabold mb-16">
+          How does it work?
+        </h1>
+        <div className="flex items-center justify-between w-full">
+          <div className=" w-[356px] h-[340px] flex-col items-center justify-center">
+            <img
+              className="w-80 h-64"
+              src="/images/h-1-x.webp"
+              alt="h1 image"
+            />
+            <p className="text-indigo-950 font-bold text-2xl my-6">
+              Choose anywhere in Europe
+            </p>
+            <p className="text-indigo-950 font-thin text-sm leading-6 w-4/5">
+              No more compromises! With us, you have an unrivaled selection of
+              cars in one place.
+            </p>
+          </div>
+          <div className=" w-[356px] h-[340px] flex-col items-center justify-center">
+            <img
+              className="w-80 h-64"
+              src="/images/h-2-x.webp"
+              alt="h1 image"
+            />
+            <p className="text-indigo-950 font-bold text-2xl my-6">
+              We'll inspect the car closely
+            </p>
+            <p className="text-indigo-950 font-thin text-sm leading-6 w-4/5">
+              A certified mechanic will thoroughly inspect your car. You will
+              decide according to the result after.
+            </p>
+          </div>
+          <div className=" w-[356px] h-[340px] flex-col items-center justify-center">
+            <img
+              className="w-80 h-64"
+              src="/images/h-3-x.webp"
+              alt="h1 image"
+            />
+            <p className="text-indigo-950 font-bold text-2xl my-6">
+              We'll deliver it to your home
+            </p>
+            <p className="text-indigo-950 font-thin text-sm leading-6 w-4/5">
+              We arrange all the paperwork, registration and delivery. All you
+              need to do is enjoy your new car.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
