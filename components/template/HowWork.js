ef5629a1f8ed7ae0f61d63246ee53cdc94b5ab59
Roqhaye Halimi <roqiahalimi95@gmail.com>
2025-12-22 16:39:07 +0330

    slider test

--- components/template/HowWork.js
+++ components/template/HowWork.js
@@ -13,64 +13,13 @@ function HowWork() {
           How does it work?
         </h1>
 
-        <div className="flex gap-4 md:grid md:grid-cols-3 overflow-x-auto md:overflow-visible snap-x snap-mandatory">
-          {/* card 1 */}
-          <div className=" w-[356px] h-[340px] flex-col items-center justify-center  min-w-full md:min-w-0 snap-center">
-            <img
-              className="w-80 h-64"
-              src="/images/h-1-x.webp"
-              alt="h1 image"
-            />
-            <p className="text-indigo-950 font-bold text-2xl my-6">
-              Choose anywhere in Europe
-            </p>
-            <p className="text-indigo-950 font-thin text-sm leading-6 w-4/5">
-              No more compromises! With us, you have an unrivaled selection of
-              cars in one place.
-            </p>
-          </div>
-          {/* card 2 */}
-          <div className=" w-[356px] h-[340px] flex-col items-center justify-center">
-            <img
-              className="w-80 h-64"
-              src="/images/h-2-x.webp"
-              alt="h1 image"
-            />
-            <p className="text-indigo-950 font-bold text-2xl my-6">
-              We'll inspect the car closely
-            </p>
-            <p className="text-indigo-950 font-thin text-sm leading-6 w-4/5">
-              A certified mechanic will thoroughly inspect your car. You will
-              decide according to the result after.
-            </p>
-          </div>
-          {/* card 3 */}
-          <div className=" w-[356px] h-[340px] flex-col items-center justify-center">
-            <img
-              className="w-80 h-64"
-              src="/images/h-3-x.webp"
-              alt="h1 image"
-            />
-            <p className="text-indigo-950 font-bold text-2xl my-6">
-              We'll deliver it to your home
-            </p>
-            <p className="text-indigo-950 font-thin text-sm leading-6 w-4/5">
-              We arrange all the paperwork, registration and delivery. All you
-              need to do is enjoy your new car.
-            </p>
-          </div>
-        </div>
         <div
-          className="
-    flex
-    gap-4
-    overflow-x-auto
-    snap-x snap-mandatory
-    md:grid md:grid-cols-3
-    md:overflow-visible
+          className="flex gap-4 overflow-x-auto snap-x snap-mandatory md:grid md:grid-cols-3 md:overflow-visible
   "
         >
-          <div className="min-w-full snap-center bg-red-200 h-40 rounded-xl">12</div>
+          <div className="min-w-full snap-center bg-red-200 h-40 rounded-xl">
+            12
+          </div>
           <div className="min-w-full snap-center bg-green-200 h-40 rounded-xl" />
           <div className="min-w-full snap-center bg-blue-200 h-40 rounded-xl" />
         </div>
