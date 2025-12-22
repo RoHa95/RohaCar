dd88c9c0fd97cd8f8b635657dd6d8c17eaf4fa05
Roqhaye Halimi <roqiahalimi95@gmail.com>
2025-12-22 19:05:59 +0330

    fix riponsivelity of howwork component

--- components/template/HowWork.js
+++ components/template/HowWork.js
@@ -13,48 +13,52 @@ function HowWork() {
           How does it work?
         </h1>
 
-        <div className="flex gap-4 md:grid md:grid-cols-3 overflow-x-auto md:overflow-visible snap-x overscroll-x-contain snap-mandatory">
+        <div className="grid grid-flow-col auto-cols-[100%] gap-4 overflow-x-auto snap-x snap-mandatory overscroll-x-contain md:grid-flow-row md:grid-cols-3 md:auto-cols-auto md:overflow-visible">
           {/* card 1 */}
-          <div className=" md:w-[356px] h-[340px] flex-col items-center justify-center  min-w-full md:min-w-0 snap-start scroll-ml-4">
+          <div className="snap-start w-full md:w-[260px] lg:w-[356px] md:h-[315px] flex flex-col items-center justify-center">
             <img
               className="w-80 h-64"
               src="/images/h-1-x.webp"
               alt="h1 image"
             />
-            <p className="text-indigo-950 font-bold text-2xl my-6">
+
+            <p className="text-indigo-950 font-bold text-2xl md:text-xl my-6">
               Choose anywhere in Europe
             </p>
-            <p className="text-indigo-950 font-thin text-sm leading-6 w-4/5">
+            <p className="text-indigo-950 font-thin text-sm text-center sm:text-left leading-6 lg:w-4/5 sm:pr-3 lg:pr-0 ">
               No more compromises! With us, you have an unrivaled selection of
               cars in one place.
             </p>
           </div>
           {/* card 2 */}
-          <div className=" md:w-[356px] h-[340px] flex-col items-center justify-center min-w-full md:min-w-0 snap-start scroll-ml-4">
+          <div className="snap-start w-full md:w-[260px] lg:w-[356px] lg:h-[340px] md:h-[300px] flex flex-col items-center justify-center">
             <img
               className="w-80 h-64"
               src="/images/h-2-x.webp"
               alt="h1 image"
             />
-            <p className="text-indigo-950 font-bold text-2xl my-6">
+
+            <p className="text-indigo-950 font-bold text-2xl md:text-xl my-6">
               We'll inspect the car closely
             </p>
-            <p className="text-indigo-950 font-thin text-sm leading-6 w-4/5">
+            <p className="text-indigo-950 font-thin  text-sm text-center sm:text-left leading-6 lg:w-4/5 sm:pr-3 lg:pr-0">
               A certified mechanic will thoroughly inspect your car. You will
               decide according to the result after.
             </p>
           </div>
           {/* card 3 */}
-          <div className=" md:w-[356px] h-[340px] flex-col items-center justify-center min-w-full md:min-w-0 snap-start scroll-ml-4">
+          <div className="snap-start w-full md:w-[260px] lg:w-[356px] md:h-[310px] lg:h-[340px] flex flex-col items-center justify-center">
             <img
-              className="w-80 h-64"
+              className="w-80 h-64 hidden md:flex"
               src="/images/h-3-x.webp"
               alt="h1 image"
             />
-            <p className="text-indigo-950 font-bold text-2xl my-6">
+            <img src="/images/h-3-m.webp" className="md:hidden" alt="" />
+
+            <p className="text-indigo-950 font-bold text-2xl md:text-xl my-6">
               We'll deliver it to your home
             </p>
-            <p className="text-indigo-950 font-thin text-sm leading-6 w-4/5">
+            <p className="text-indigo-950 font-thin text-sm text-center sm:text-left leading-6 lg:w-4/5 sm:pr-3 lg:pr-0">
               We arrange all the paperwork, registration and delivery. All you
               need to do is enjoy your new car.
             </p>
